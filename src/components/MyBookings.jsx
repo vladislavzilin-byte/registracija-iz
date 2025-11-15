932adbda6973a18afa6beaa5f00c506006c9c9f5
vladislavzilin-byte <vladislavzilin@gmail.com>
2025-11-15 14:16:46 +0200

    Update MyBookings.jsx

--- src/components/MyBookings.jsx
+++ src/components/MyBookings.jsx
@@ -2,6 +2,22 @@ import { useEffect, useState } from "react"
 import { getBookings, saveBookings, getCurrentUser } from "../lib/storage"
 import { useI18n } from "../lib/i18n"
 
+const SERVICE_PRICES = {
+  "Šukuosena": 50,
+  "Tresų nuoma": 25,
+  "Papuošalų nuoma": 10,
+  "Atvykimas": 50,
+  "Konsultacija": 10,
+}
+
+const SERVICE_DURATIONS = {
+  "Šukuosena": 60,      // 1 час
+  "Tresų nuoma": 15,    // 15 мин
+  "Papuošalų nuoma": 15,// 15 мин
+  "Atvykimas": 120,     // 2 часа
+  "Konsultacija": 30,   // 30 мин
+}
+
 export default function MyBookings() {
   const { t } = useI18n()
   const user = getCurrentUser()
