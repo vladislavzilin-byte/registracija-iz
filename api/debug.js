3febb0043875a51c55c1a9af8f40dd1e658bc071
vladislavzilin-byte <vladislavzilin@gmail.com>
2025-12-01 20:06:20 +0200

    Update debug.js

--- api/debug.js
+++ api/debug.js
@@ -1,7 +1,11 @@
 export default function handler(req, res) {
   res.json({
     SMTP_USER: process.env.SMTP_USER || "MISSING",
-    SMTP_PASS: process.env.SMTP_PASS ? "SET" : "MISSING",
-    HOST: process.env.VERCEL_URL || null
+    SMTP_PASS: process.env.SMTP_PASS || "MISSING",
+    SMTP_PORT: process.env.SMTP_PORT || "MISSING",
+    SMTP_SECURE: process.env.SMTP_SECURE || "MISSING",
+    FROM_EMAIL: process.env.FROM_EMAIL || "MISSING",
+    FROM_NAME: process.env.FROM_NAME || "MISSING",
+    RUNTIME_HOST: req.headers.host || null
   })
 }
