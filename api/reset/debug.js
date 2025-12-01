f18038ec31241b38d3d81b801914284019f49f70
vladislavzilin-byte <vladislavzilin@gmail.com>
2025-12-01 22:34:38 +0200

    Delete api/reset/debug.js

--- api/reset/debug.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.json({
-    SMTP_USER: process.env.SMTP_USER || "MISSING",
-    SMTP_PASS: process.env.SMTP_PASS ? "SET" : "MISSING",
-    HOST: process.env.VERCEL_URL || null
-  })
-}
