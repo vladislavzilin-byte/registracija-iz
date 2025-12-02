517827b222f166180e9c7f7cb80024081a504243
vladislavzilin-byte <vladislavzilin@gmail.com>
2025-12-02 18:41:13 +0200

    Update ForgotPasswordModal.jsx

--- src/components/ForgotPasswordModal.jsx
+++ src/components/ForgotPasswordModal.jsx
@@ -15,7 +15,7 @@ export default function ForgotPasswordModal({ open, onClose }){
     if(!u) setFound({error:'Пользователь не найден'})
     else setFound({password:u.password})
   }
-
+ 
   return (
     <div style={{
       position:'fixed', top:0, left:0, width:'100%', height:'100%',
