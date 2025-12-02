9c09c2c239a37e94b3a54e7ddf0823cbcfccb2b4
vladislavzilin-byte <vladislavzilin@gmail.com>
2025-12-02 10:14:46 +0200

    Update verify-code.js

--- api/reset/verify-code.js
+++ api/reset/verify-code.js
@@ -25,11 +25,9 @@ export default async function handler(req, res) {
       return res.status(400).json({ ok: false, error: "wrong_code" });
     }
 
-    // Удаляем после использования
     await redis.del(`reset:${normalizedEmail}`);
 
     return res.status(200).json({ ok: true });
-
   } catch (err) {
     console.error("VERIFY ERROR:", err);
     return res.status(500).json({ ok: false });
