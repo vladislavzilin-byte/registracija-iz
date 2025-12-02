c7740daf1359ce152c479a56d1a91161f4723134
vladislavzilin-byte <vladislavzilin@gmail.com>
2025-12-02 20:16:34 +0200

    Update send-code.js

--- api/reset/send-code.js
+++ api/reset/send-code.js
@@ -41,8 +41,7 @@ export default async function handler(req, res) {
 
   // 📌 Правильный, стабильный URL (PNG → лучше для email)
 // Рабочий RAW URL (именно с "?raw=1")
-const logoUrl =
-  "https://registracija-iz.vercel.app/logo-email.png";
+const logoUrl = "https://registracija-iz.vercel.app/logo-email.png";
 
   try {
     const transporter = nodemailer.createTransport({
